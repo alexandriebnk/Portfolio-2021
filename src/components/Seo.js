7c2bed9820b350fe9a1965f6f44db70d7e26a9c2
alexandriebnk <alexandrie.be@gmail.com>
2021-10-12 19:54:30 +0200

    Update Helmet SEO

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -8,22 +8,38 @@ const Seo = () => {
   return (
     <Helmet>
       <html lang="fr" />
-      <title>
-        {"Alexandrie Benkiki - Développeur Front-End" || data?.seo.title}
-      </title>
-      <meta name="author" content={data?.seo.author} />
-      <meta name="description" content={data?.seo.description} />
+      <title>Alexandrie Benkiki - Développeur Front-End</title>
+      <meta name="author" content="Alexandrie Benkiki" />
+      <meta
+        name="description"
+        content="Portfolio - Alexandrie Benkiki, développeur Front-End. Technologies : HTML/CSS, SASS, Javascript, ReactJS, GSAP, Webpack, Figma."
+      />
 
-      <meta property="og:url" content={"http://www.alexandrie.dev"} />
-      <meta property="og:title" content={data?.seo.title} />
-      <meta name="og:description" content={data?.seo.description} />
-      <meta property="og:image" content={"http://www.alexandrie.dev/og.png"} />
+      <meta property="og:url" content={"https://www.alexandrie.dev"} />
+      <meta
+        property="og:title"
+        content="Alexandrie Benkiki - Développeur Front-End"
+      />
+      <meta
+        name="og:description"
+        content="Portfolio - Alexandrie Benkiki, développeur Front-End. Technologies : HTML/CSS, SASS, Javascript, ReactJS, GSAP, Webpack, Figma."
+      />
+      <meta property="og:image" content={"https://www.alexandrie.dev/og.png"} />
 
-      <meta name="twitter:description" content={data?.seo.description} />
-      <meta name="twitter:title" content={data?.seo.title} />
-      <meta name="twitter:creator" content={data?.seo.author} />
+      <meta
+        name="twitter:description"
+        content="Portfolio - Alexandrie Benkiki, développeur Front-End. Technologies : HTML/CSS, SASS, Javascript, ReactJS, GSAP, Webpack, Figma."
+      />
+      <meta
+        name="twitter:title"
+        content="Alexandrie Benkiki - Développeur Front-End"
+      />
+      <meta name="twitter:creator" content="Alexandrie Benkiki" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image" content={"http://www.alexandrie.dev/og.png"} />
+      <meta
+        name="twitter:image"
+        content={"https://www.alexandrie.dev/og.png"}
+      />
     </Helmet>
   );
 };
